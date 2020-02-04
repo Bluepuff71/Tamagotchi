1095f15437f0d2f92ac369089c006330b4f3fb63
Emery Porter <emeryporter99@gmail.com>
2020-02-03 23:37:03 -0600

    Built the game loop
    ~Also began working on the game UI

---
+++ src/Game.java
@@ -0,0 +1,42 @@
+public class Game implements Runnable {
+
+    private Tamagotchi tamagotchi;
+    /**
+     * Starts up the Game Thread.
+     */
+    public Game(Tamagotchi tamagotchi){
+        this.tamagotchi = tamagotchi;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(100);
+                update();
+            }
+        } catch (Exception e) {
+            System.out.println("Big Error. Tell Emery!");
+        }
+    }
+
+    /**
+     * The heartbeat of the game. Runs every 100ms.
+     */
+    private void update(){
+        tamagotchi.decrimentStats();
+    }
+
+    //Text based UI Methods
+    private void gameUI(){
+        System.out.println(tamagotchi); //Print the tamagotchi stats (toString)
+        System.out.println("What would you like to do?");
+        System.out.println("[1] Feed");
+        System.out.println("[2] Water");
+        System.out.println("[3] Play");
+        System.out.println("[4] Quit");
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    //Main Method, we can ignore
+    public static void main(String[] args){
+        new Main();
+    }
+
+    public Main(){
+        mainMenu();
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    void mainMenu(){
+        System.out.println("Tamagotchi V0.0.1");
+        System.out.println("[1] Create a Tamagotchi");
+        System.out.println("[2] Load a Tamagotchi");
+        System.out.println("[3] Quit");
+        while (!scanner.hasNextInt()){
+            System.out.println("That is not an option.");
+            mainMenu();
+        }
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:{
+                createTamagotchi();
+            }
+            case 2:{
+                loadTamagotchi();
+            }
+            case 3:{
+                //quit
+            }
+            default:{
+                System.out.println("That is not an option.");
+                mainMenu();
+            }
+        }
+    }
+
+    private void createTamagotchi(){
+        System.out.println("Time to create your Tamagotchi!");
+        System.out.println("First, you need to name it!");
+        System.out.println("Enter its name: ");
+        String name = scanner.nextLine();
+        System.out.println("Perfect!");
+        System.out.println("Creating your Tamagotchi...");
+        new Game(new Tamagotchi(name));
+    }
+
+    private void loadTamagotchi(){
+
+    }
+}
