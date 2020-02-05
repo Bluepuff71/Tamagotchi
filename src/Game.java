d378cf83f0b79f72017f698ba99a6b26aef7a175
Emery Porter <emeryporter99@gmail.com>
2020-02-04 20:51:22 -0600

    Cleared for rewrite
    ~ Needs to be rewritten with new GUI class

--- src/Game.java
+++ src/Game.java
@@ -1,45 +1,9 @@
-public class Game implements Runnable {
+import java.awt.*;
 
-    private Tamagotchi tamagotchi;
-    /**
-     * Starts up the Game Thread.
-     */
-    public Game(Tamagotchi tamagotchi){
-        this.tamagotchi = tamagotchi;
-        new Thread(this).start();
-        gameUI();
-    }
+public class Game extends GUI {
 
     @Override
-    public void run() {
-        try {
-            while (true) {
-                Thread.sleep(100);
-                update();
-            }
-        } catch (Exception e) {
-            System.out.println("Big Error. Tell Emery!");
-        }
+    public void draw(Graphics g, Input input) {
+
     }
-
-    /**
-     * The heartbeat of the game. Runs every 100ms.
-     */
-    private void update(){
-        tamagotchi.decrimentStats();
-    }
-
-    //Text based UI Methods
-    private void gameUI(){
-        System.out.println(tamagotchi); //Print the tamagotchi stats (toString)
-        System.out.println("What would you like to do?");
-        System.out.println("[1] Feed");
-        System.out.println("[2] Water");
-        System.out.println("[3] Play");
-        System.out.println("[4] Clean");
-        System.out.println("[5] Talk");
-        System.out.println("[6] Quit");
-    }
-
-
 }
