b7bcac5a624a84dde632efa448b611700caa4fd0
Dalton Eschberger <eschberger.dalton@gmail.com>
2020-02-12 15:28:59 -0600

    added todo

--- src/game/Communicate.java
+++ src/game/Communicate.java
@@ -5,6 +5,9 @@ import game.core.enums.Personality;
 import game.core.enums.speech.NormalSpeech;
 import game.core.enums.speech.NotNormalSpeech;
 
+//TODO: create serialization/deserialization 
+//TODO: change from array to hashmap
+
 //created by Dalton
 /*
 This class is used to get the speech options for a given time in the game - based on personality and current mood
